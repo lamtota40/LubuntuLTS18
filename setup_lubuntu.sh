0f1dbcdd2c6599990edd9829b4577bb89467a0d8
lamtota40 <lamtota40@gmail.com>
2025-05-08 13:22:11 +0700

    Update setup_lubuntu.sh

--- setup_lubuntu.sh
+++ setup_lubuntu.sh
@@ -1,3 +1,20 @@
-partisi
+#!/bin/bash
+
+sudo apt update && sudo apt upgrade
+sudo apt install openssh-server -y
+sudo apt install cpu-x -y
+sudo snap install snap-store
+sudo snap install notepad-plus-plus
+
+sudo pkill firefox || true
+sudo add-apt-repository -y ppa:mozillateam/ppa
+sudo apt update
+    
+sudo apt install firefox -y
+
+sudo apt autoremove -y
+sudo systemctl stop lightdm
+sudo systemctl disable lightdm
+reboot
 
 https://cdimage.ubuntu.com/lubuntu/releases/18.04/release/
