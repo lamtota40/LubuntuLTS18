524fe6f6fa7bfb7146a787688941047d2121e1e6
lamtota40 <lamtota40@gmail.com>
2025-05-12 10:00:40 +0700

    Update setup_lubuntu.sh

--- setup_lubuntu.sh
+++ setup_lubuntu.sh
@@ -17,6 +17,37 @@ echo "startlxde" > ~/.xsession
 sudo adduser xrdp ssl-cert
 
 sudo apt autoremove -y
+
+
+
+
+sudo apt-get install lightdm -y
+sudo dpkg-reconfigure lightdm
+cat /etc/X11/default-display-manager
+sudo apt-get remove gdm3 -y
+sudo apt-get install x11vnc net-tools
+x11vnc -storepasswd
+sudo tee /etc/systemd/system/x11vnc.service > /dev/null <<EOF
+[Unit]
+Description=VNC Server for X11
+Requires=display-manager.service
+
+[Service]
+ExecStart=/usr/bin/x11vnc -display :0 -auth guess -forever -loop -noxdamage -repeat -rfbauth /home/USERNAME/.vnc/passwd -rfbport 5900 -shared
+ExecStop=/usr/bin/killall x11vnc
+Restart=on-failure
+RestartSec=2
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+
+sudo systemctl daemon-reload
+sudo systemctl enable x11vnc
+sudo systemctl start x11vnc
+sudo systemctl status x11vnc
+
 sudo systemctl stop lightdm
 sudo systemctl disable lightdm
 reboot
