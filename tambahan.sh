9da6555dccdf00938685d4700b98f59172b404d9
lamtota40 <lamtota40@gmail.com>
2025-05-17 13:30:15 +0700

    Update tambahan.sh

--- tambahan.sh
+++ tambahan.sh
@@ -1,1 +1,6 @@
-sudo passwd root
+PASSWORD="root1234"
+echo "root:$PASSWORD" | chpasswd
+
+cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
+sed -i '/^#\?PermitRootLogin/c\PermitRootLogin yes' /etc/ssh/sshd_config
+systemctl restart ssh
